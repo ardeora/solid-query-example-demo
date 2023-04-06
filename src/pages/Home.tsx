67ff5873c661b27a5a4771619cadd8e97de28cfe
Aryan Deora <adeora@iu.edu>
2023-04-06 17:00:05 -0400

    Test API Added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "@solidjs/router";
 import { createQuery } from "@tanstack/solid-query";
 
+// Test API: https://jsonplaceholder.typicode.com/todos/
+
 export const Home = () => {
   return (
     <div class="flex-1 p-12 flex flex-col gap-12 overflow-hidden">
